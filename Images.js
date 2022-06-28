e6f428acb651182584f969a3f945e7579a71df55
VysaliN <105570788+VysaliN@users.noreply.github.com>
2022-06-28 11:41:19 +0530

    Add files via upload

---
+++ Images.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Picture from "./image.jpg";
+
+const Images = () => {
+  return (
+    <div>
+      <center>
+        <img src={Picture} height="200" width="200" />
+      </center>
+    </div>
+  );
+};
+
+export default Images;
