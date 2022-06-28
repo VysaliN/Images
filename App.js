d141ae498e5c3843aa2779a2fe1de6ba1be5cd7f
VysaliN <105570788+VysaliN@users.noreply.github.com>
2022-06-28 11:45:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import Images from './Images';
+
+const App = () => {
+  return (
+    <div>
+      <Images/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React,{useState} from 'react';
+// import Data from './state.json';
+
+// const App = () => {
+//     const [search,setSearch] = useState('');
+//   return (
+//     <div>
+//         <center>
+//         <h2>Enter Your state:</h2><br></br>
+//         <input type="text"  value={search} onChange={(e)=> setSearch(e.target.value)}/><br></br><br></br>
+//         {Data.filter(state => state.name.includes(search)).map(state =>{
+//             return <div style={{"border":"1px solid black","padding":"5px","margin":"5px","width":"50%"}}>
+//                 {state.name}
+//             </div>
+// })}
+// </center>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// import React from 'react'
+// import File from './File'
+
+// const App = () => {
+//   return (
+//     <div>
+//         <File />
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from "react";
+// import Audio from "./Audio";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Audio />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import React from "react";
+// import Video from "./Video";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Video />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import React from "react";
+// import Images from "./Images";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Images />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import React from 'react'
+// import Calculator from './Calculator'
+// import './Calculator.css'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Calculator />
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+// import Form from './Form'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Form />
+//     </div>
+//   )
+// }
+// export default App;
